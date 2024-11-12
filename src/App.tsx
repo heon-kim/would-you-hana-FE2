172d5fd5bcb2a7dd31fe23359ed0db9ae6e37e12
freamwork97 <jsy11123@naver.com>
2024-11-13 08:59:09 +0900

    UJU-35 챗봇 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ConfigProvider } from 'antd';
-import Home from './pages/Home/Home';
-import MyPage from './pages/MyPage/MyPage';
-import Profile from './pages/MyPage/Profile';
-import Posts from './pages/MyPage/Posts';
-import Likes from './pages/MyPage/Likes';
-import Scrap from './pages/MyPage/Scraps';
-import EditProfile from './pages/MyPage/EditProfile';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ConfigProvider } from "antd";
+import Home from "./pages/Home/Home";
+import MyPage from "./pages/MyPage/MyPage";
+import Profile from "./pages/MyPage/Profile";
+import Posts from "./pages/MyPage/Posts";
+import Likes from "./pages/MyPage/Likes";
+import Scrap from "./pages/MyPage/Scraps";
+import EditProfile from "./pages/MyPage/EditProfile";
 // import Auth from "./pages/Auth";
-import Login from './pages/Auth/Login';
-import Register from './pages/Auth/Register';
-import FindPassword from './pages/Auth/FindPassword';
-import './App.css';
-import './styles/fonts.css';
-import Header from './components/Header';
-import QuestionDetail from './pages/QnA/QuestionDetail';
-import QuestionRegister from './pages/QnA/QuestionRegister';
-import Board from './pages/QnA/Board';
-import FindBank from './pages/Location/FindBank';
-import NotFound from './pages/Error/404';
-import Community from './pages/Community/Community';
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import FindPassword from "./pages/Auth/FindPassword";
+import "./App.css";
+import "./styles/fonts.css";
+import Header from "./components/Header";
+import QuestionDetail from "./pages/QnA/QuestionDetail";
+import QuestionRegister from "./pages/QnA/QuestionRegister";
+import Board from "./pages/QnA/Board";
+import FindBank from "./pages/Location/FindBank";
+import NotFound from "./pages/Error/404";
+import Community from "./pages/Community/Community";
 
-import Gwangjin from './pages/LandingPage/Gwangjin';
-import Seocho from './pages/LandingPage/Seocho';
+import Gwangjin from "./pages/LandingPage/Gwangjin";
+import Seocho from "./pages/LandingPage/Seocho";
 
-import BankerProfile from './pages/BankerProfile/Profile';
-import CommunityRegister from './pages/Community/CommunityRegister';
+import BankerProfile from "./pages/BankerProfile/Profile";
+import CommunityRegister from "./pages/Community/CommunityRegister";
+import Chatbot from "./components/chatbot";
 
 function App() {
   return (
     <ConfigProvider
       theme={{
         token: {
-          colorPrimary: '#008485',
+          colorPrimary: "#008485",
         },
       }}
     >
@@ -63,6 +64,8 @@ function App() {
             <Route path="/gwangjin" element={<Gwangjin />}></Route>
             <Route path="/seocho" element={<Seocho />}></Route>
             <Route path="/bankerProfile" element={<BankerProfile />}></Route>
+            <Route path="/chatbot" element={<Chatbot />}></Route>
+
             {/* Error */}
             <Route path="/404" element={<NotFound />} />
           </Routes>
