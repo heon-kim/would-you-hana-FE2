da54ec292dda15e16fe9a64732c73774ff241352
heon-kim <vndtka123@gmail.com>
2024-11-17 23:16:53 +0900

    chore: 헤더 지역 선택지를 favoriteLocations로 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,58 +1,15 @@
-import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import logo from '../assets/img/logo.png';
 import userIcon from '../assets/img/icon_user.png';
 import locationIcon from '../assets/img/icon_location.svg';
 import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from '../hoc/store';
+import store, { RootState } from '../hoc/store';
 import { logout } from '../hoc/actions';
-import { DownOutlined } from '@ant-design/icons';
-import { Dropdown, Space, message, Select } from 'antd';
-import type { MenuProps, SelectProps } from 'antd';
-import { setAuthHeader, setUserRole, setUserEmail, getUserLocation } from '../hoc/request';
+import { message, Select } from 'antd';
+import type { SelectProps } from 'antd';
+import { getUserLocation } from '../hoc/request';
 import { findUser, findBanker} from '../utils/userStorage';
-
-// SearchInput 컴포넌트에 사용할 로컬 데이터
-const items = [
-  // { value: '강서구', text: '강서구' },
-  // { value: '양천구', text: '양천구' },
-  // { value: '구로구', text: '구로구' },
-  // { value: '금천구', text: '금천구' },
-  // { value: '영등포구', text: '영등포구' },
-  // { value: '동작구', text: '동작구' },
-  // { value: '관악구', text: '관악구' },
-  { value: '서초구', text: '서초구' },
-  // { value: '강남구', text: '강남구' },
-  // { value: '송파구', text: '송파구' },
-  // { value: '강동구', text: '강동구' },
-  // { value: '마포구', text: '마포구' },
-  // { value: '서대문구', text: '서대문구' },
-  // { value: '은평구', text: '은평구' },
-  // { value: '종로구', text: '종로구' },
-  // { value: '성북구', text: '성북구' },
-  // { value: '동대문구', text: '동대문구' },
-  // { value: '중구', text: '중구' },
-  { value: '광진구', text: '광진구' },
-  // { value: '중랑구', text: '중랑구' },
-  // { value: '용산구', text: '용산구' },
-  // { value: '강북구', text: '강북구' },
-  // { value: '도봉구', text: '도봉구' },
-  // { value: '노원구', text: '노원구' },
-  // { value: '성동구', text: '성동구' },
-
-  // 필요한 다른 데이터 추가
-];
-
-// 검색 기능을 수행하는 함수
-const fetch = (
-  value: string,
-  callback: (data: { value: string; text: string }[]) => void
-) => {
-  const filteredData = items.filter((item) =>
-    item.text.toLowerCase().includes(value.toLowerCase())
-  );
-  callback(filteredData);
-};
 
 // SearchInput 컴포넌트
 const SearchInput: React.FC<{
@@ -62,12 +19,29 @@ const SearchInput: React.FC<{
   onChange: (newValue: string) => void;
 }> = (props) => {
   const [data, setData] = useState<SelectProps['options']>([]);
+  const [favoriteLocations, setFavoriteLocations] = useState<string[]>([]);
+
+  useEffect(() => {
+    const loggedUser = localStorage.getItem('userEmail');
+    if (loggedUser) {
+      const user = findUser(loggedUser);
+      setFavoriteLocations(user?.favoriteLocations || ['광진구']);
+    } else {
+      setFavoriteLocations(['광진구']);
+    }
+  }, []);
 
   const navigate = useNavigate();
 
-  const handleSearch = (newValue: string) => {
-    fetch(newValue, setData);
+  const handleSearch = async (value: string) => {
+    const filteredData = favoriteLocations
+      .map((loc) => {
+        return { text: loc, value: loc };
+      })
+      .filter((item) => item.text.toLowerCase().includes(value.toLowerCase()));
+    await setData(filteredData);
   };
+      
 
   const navigateToLanding = (district : string) => {
     console.log(district);
@@ -97,7 +71,7 @@ const SearchInput: React.FC<{
         onSearch={handleSearch}
         onChange={handleChange}
         notFoundContent={null}
-        options={(data || []).map((d) => ({
+        options={data?.map((d) => ({
           value: d.value,
           label: d.text,
         }))}
@@ -116,13 +90,12 @@ interface LoggedInComponentProps {
 }
 
 function LoggedInComponent({ onLogout, loggedInUserType }: LoggedInComponentProps) {
-  if(loggedInUserType =='C') {
     const loggedUser = localStorage.getItem('userEmail');
-    let user;
+    let user, banker;
     if (loggedUser) {
-      user = findUser(loggedUser);
+      user = loggedInUserType == 'C' ? findUser(loggedUser) : undefined;
+      banker = loggedInUserType == 'B' ? findBanker(loggedUser) : undefined;
     }
-
     return (
       <div>
         <ul className='flex gap-8 items-center '>
@@ -134,64 +107,13 @@ function LoggedInComponent({ onLogout, loggedInUserType }: LoggedInComponentProp
           <li>
             <Link to="/my/profile" className="flex items-center gap-2">
               <img src={userIcon} alt="user icon" width={35} />
-              <span>{user?.nickname}</span>
+              {user && (<span>{user.nickname}</span>)}
+              {banker && (<span>{banker.name}</span>)}
             </Link>
           </li>
         </ul>
       </div>
     );
-  }
-
-  else if(loggedInUserType == 'B') {
-    const loggedBanker = localStorage.getItem('userEmail');
-    let banker;
-    if (loggedBanker) {
-      banker = findBanker(loggedBanker);
-    }
-
-    return (
-      <div>
-        <ul className='flex gap-8 items-center '>
-          <li>
-            <span onClick={onLogout} style={{ cursor: 'pointer' }}>
-              로그아웃
-            </span>
-          </li>
-          <li>
-            <Link to="/my/profile" className="flex items-center gap-2">
-              <img src={userIcon} alt="user icon" width={35} />
-              <span>{banker?.name}</span>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-  // const loggedUser = localStorage.getItem('loggedUser');
-  //   let user;
-  //   if (loggedUser) {
-  //     user = findUser(loggedUser);
-  //   }
-
-  //   return (
-  //     <div>
-  //       <ul className='flex gap-8 items-center '>
-  //         <li>
-  //           <span onClick={onLogout} style={{ cursor: 'pointer' }}>
-  //             로그아웃
-  //           </span>
-  //         </li>
-  //         <li>
-  //           <Link to="/my/profile" className="flex items-center gap-2">
-  //             <img src={userIcon} alt="user icon" width={35} />
-  //             <span>{user?.nickname}</span>
-  //           </Link>
-  //         </li>
-  //       </ul>
-  //     </div>
-  //   );
-  
-  
 }
 
 function LoggedOutComponent() {
@@ -215,7 +137,7 @@ function Header() {
   const [loggedInType, setLoggedInType] = useState<string|null>(null);
   const [searchValue, setSearchValue] = useState<string>('');
   const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<typeof store.dispatch>();
   const userLocation = getUserLocation(); //사용자의 초기 위치값
 
   const isAuthenticated = useSelector(
@@ -227,17 +149,11 @@ function Header() {
   );
 
   useEffect(() => {
-    setIsLoggedIn(isAuthenticated);
-    setLoggedInType(loggedInUserType);
+      setIsLoggedIn(isAuthenticated);
+      setLoggedInType(loggedInUserType);
   }, [isAuthenticated, loggedInUserType]);
 
   const handleLogout = () => {
-    // localStorage.removeItem('loggedUser');
-    // setUserRole(null);
-    // setUserEmail(null);
-    // setAuthHeader(null);
-    // setIsLoggedIn(false);
-    // localStorage.removeItem('authToken');
     dispatch(logout());
     message.success('로그아웃 성공!');
     navigate('/');
@@ -272,10 +188,12 @@ function Header() {
           </ul>
         </nav>
 
-        <div className='flex ml-auto mr-10 gap-3 items-center'>
+        {loggedInType !== 'B' && (
+          <div className='flex ml-auto mr-10 gap-3 items-center'>
           {/* 검색한 위치가 없는 경우, userLocation값이 입력됨 */}
-          <SearchInput placeholder='지역을 입력하세요' style={{ width: 200 }} value={searchValue || userLocation} onChange={handleSearchValueChange} />
+          <SearchInput placeholder='지역을 입력하세요' style={{ width: 200 }} value={searchValue || userLocation || ''} onChange={handleSearchValueChange} />
         </div>
+        )}
 
         {isLoggedIn ? (
           <LoggedInComponent onLogout={handleLogout} loggedInUserType={loggedInType} />
