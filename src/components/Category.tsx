9ccd86e1838fb1db7c5ff02cd1ae09c5e37810e0
codnscodns <dnstm1119@naver.com>
2024-11-18 10:51:01 +0900

    ui: QnA게시판 카테고리 아이콘 추가

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
 import { Categories } from '../constants/posts';
+import IconViewAll from '../assets/img/category/icon_view_all.png';
+import IconDeposit from '../assets/img/category/icon_deposit_saving.png';
+import IconTransfer from '../assets/img/category/icon_transfer.png';
+import IconAssetManagement from '../assets/img/category/icon_asset_management.png';
+import IconRetirementPension from '../assets/img/category/icon_retirement_pension.png';
+import IconFund from '../assets/img/category/icon_fund.png';
+import IconTrust from '../assets/img/category/icon_trust.png';
+import IconISA from '../assets/img/category/icon_isa.png';
+import IconMobileBanking from '../assets/img/category/icon_mobile_banking.svg';
+import IconLoan from '../assets/img/category/icon_loan.png';
+import IconForeignExchange from '../assets/img/category/icon_foreign_exchange.png';
+import IconInsurance from '../assets/img/category/icon_insurance.svg';
+import IconCard from '../assets/img/category/icon_card.png';
+import IconETC from '../assets/img/category/icon_etc.png';
+
 interface Category {
   name: string;
   icon: string;
@@ -8,8 +23,25 @@ interface CategoryProps {
   onSelectCategory: (category: string) => void;
 }
 
+const categoryIcons = [
+  IconViewAll,
+  IconDeposit,
+  IconTransfer,
+  IconAssetManagement,
+  IconRetirementPension,
+  IconFund,
+  IconTrust,
+  IconISA,
+  IconMobileBanking,
+  IconLoan,
+  IconForeignExchange,
+  IconInsurance,
+  IconCard,
+  IconETC
+]
+
 const categoriesWithIcon = ['전체', ...Categories].map((category, index) => {
-  return { name: category, icon: index + 1 };
+  return { name: category, icon: categoryIcons[index % categoryIcons.length] };
 });
 
 const Category: React.FC<CategoryProps> = ({ onSelectCategory }) => {
@@ -43,7 +75,11 @@ const Category: React.FC<CategoryProps> = ({ onSelectCategory }) => {
                 }} // 카테고리 선택 시 호출
               >
                 <div className="w-full h-14 bg-gray-100 rounded-xl flex items-center justify-center hover:bg-[#DDFCD2] transition-colors duration-300">
-                  {category.icon}
+                  <img
+                    src={category.icon}
+                    alt={category.name}
+                    className="w-8 h-8"
+                  />
                 </div>
                 <p className="whitespace-nowrap">{category.name}</p>
               </button>
