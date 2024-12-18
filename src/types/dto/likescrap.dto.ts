a5541c340b7fd549474bdf44d460f54b68a50310
codnscodns <dnstm1119@naver.com>
2024-12-18 15:35:56 +0900

    chore: likescrap.dto 수정

---
+++ src/types/dto/likescrap.dto.ts
@@ -0,0 +1,10 @@
+export interface LikesScrapDTO{
+    requestId : number;
+    questionId : number;
+    categoryName : string;
+    title : string;
+    likeCount : number;
+    viewCount : number;
+    createdAt : string;
+    bankerName : string;
+}
